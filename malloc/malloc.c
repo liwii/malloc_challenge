108ac004e159a3d417fc7fe6bb3e2913e9ae3a6c
Koki Ryu <liukoki@gmail.com>
2022-06-22 22:00:24 +0900

    Free bins

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -25,6 +25,9 @@ void munmap_to_system(void *ptr, size_t size);
 // Struct definitions
 //
 
+const int MIN_BIN_SCALE = 3;
+const int MAX_BIN_SCALE = 12;
+
 typedef struct my_metadata_t
 {
   size_t size;
@@ -33,7 +36,8 @@ typedef struct my_metadata_t
 
 typedef struct my_heap_t
 {
-  my_metadata_t *free_head;
+  // free_bins[i] contains the list of free spaces whose size <= (1 << (i + MIN_BIN_SCALE))
+  my_metadata_t *free_bins[MAX_BIN_SCALE - MIN_BIN_SCALE + 1];
   my_metadata_t dummy;
 } my_heap_t;
 
@@ -46,14 +50,27 @@ my_heap_t my_heap;
 // Helper functions (feel free to add/remove/edit!)
 //
 
+int find_bin_idx(size_t size)
+{
+  for (int i = MIN_BIN_SCALE; i < MAX_BIN_SCALE; i++)
+  {
+    if ((1 << i) >= size)
+    {
+      return i - MIN_BIN_SCALE;
+    }
+  }
+  return MAX_BIN_SCALE - MIN_BIN_SCALE;
+}
+
 void my_add_to_free_list(my_metadata_t *metadata)
 {
   assert(!metadata->next);
-  metadata->next = my_heap.free_head;
-  my_heap.free_head = metadata;
+  int bin_idx = find_bin_idx(metadata->size);
+  metadata->next = my_heap.free_bins[bin_idx];
+  my_heap.free_bins[bin_idx] = metadata;
 }
 
-void my_remove_from_free_list(my_metadata_t *metadata, my_metadata_t *prev)
+void my_remove_from_free_list(my_metadata_t *metadata, my_metadata_t *prev, int idx)
 {
   if (prev)
   {
@@ -61,7 +78,7 @@ void my_remove_from_free_list(my_metadata_t *metadata, my_metadata_t *prev)
   }
   else
   {
-    my_heap.free_head = metadata->next;
+    my_heap.free_bins[idx] = metadata->next;
   }
   metadata->next = NULL;
 }
@@ -73,7 +90,10 @@ void my_remove_from_free_list(my_metadata_t *metadata, my_metadata_t *prev)
 // This is called at the beginning of each challenge.
 void my_initialize()
 {
-  my_heap.free_head = &my_heap.dummy;
+  for (int i = 0; i < MAX_BIN_SCALE - MIN_BIN_SCALE; i++)
+  {
+    my_heap.free_bins[i] = &my_heap.dummy;
+  }
   my_heap.dummy.size = 0;
   my_heap.dummy.next = NULL;
 }
@@ -84,27 +104,39 @@ void my_initialize()
 // mmap_from_system() / munmap_to_system().
 void *my_malloc(size_t size)
 {
-  my_metadata_t *metadata = my_heap.free_head;
-  my_metadata_t *prev = NULL;
-  my_metadata_t *metadata_best = metadata;
-  my_metadata_t *prev_best = prev;
-  size_t size_best = 0;
-
-  while (metadata)
+  int bin_idx = find_bin_idx(size);
+  my_metadata_t *metadata, *prev, *metadata_best, *prev_best;
+  size_t size_best;
+  int bin_idx_best = -1;
+  for (int i = bin_idx; i <= MAX_BIN_SCALE - MIN_BIN_SCALE; i++)
   {
-    if (metadata->size >= size && (size_best == 0 || size_best > metadata->size))
+    metadata = my_heap.free_bins[i];
+    prev = NULL;
+    metadata_best = metadata;
+    prev_best = prev;
+    size_best = 0;
+
+    while (metadata)
     {
-      size_best = metadata->size;
-      metadata_best = metadata;
-      prev_best = prev;
+      if (metadata->size >= size && (size_best == 0 || size_best > metadata->size))
+      {
+        size_best = metadata->size;
+        metadata_best = metadata;
+        prev_best = prev;
+      }
+      prev = metadata;
+      metadata = metadata->next;
     }
-    prev = metadata;
-    metadata = metadata->next;
+    if (size_best > 0)
+    {
+      bin_idx_best = i;
+      break;
+    }
   }
   // now, metadata points to the first free slot
   // and prev is the previous entry.
 
-  if (size_best == 0)
+  if (bin_idx_best == -1)
   {
     // There was no free slot available. We need to request a new memory region
     // from the system by calling mmap_from_system().
@@ -133,7 +165,7 @@ void *my_malloc(size_t size)
   size_t remaining_size = metadata_best->size - size;
   metadata_best->size = size;
   // Remove the free slot from the free list.
-  my_remove_from_free_list(metadata_best, prev_best);
+  my_remove_from_free_list(metadata_best, prev_best, bin_idx_best);
 
   if (remaining_size > sizeof(my_metadata_t))
   {
