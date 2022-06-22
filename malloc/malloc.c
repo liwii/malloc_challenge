164c2187e8219016f5eef72ff4e1d768ab2b7b53
Koki Ryu <liukoki@gmail.com>
2022-06-22 20:20:01 +0900

    naive best-fit malloc

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -25,12 +25,14 @@ void munmap_to_system(void *ptr, size_t size);
 // Struct definitions
 //
 
-typedef struct my_metadata_t {
+typedef struct my_metadata_t
+{
   size_t size;
   struct my_metadata_t *next;
 } my_metadata_t;
 
-typedef struct my_heap_t {
+typedef struct my_heap_t
+{
   my_metadata_t *free_head;
   my_metadata_t dummy;
 } my_heap_t;
@@ -44,16 +46,21 @@ my_heap_t my_heap;
 // Helper functions (feel free to add/remove/edit!)
 //
 
-void my_add_to_free_list(my_metadata_t *metadata) {
+void my_add_to_free_list(my_metadata_t *metadata)
+{
   assert(!metadata->next);
   metadata->next = my_heap.free_head;
   my_heap.free_head = metadata;
 }
 
-void my_remove_from_free_list(my_metadata_t *metadata, my_metadata_t *prev) {
-  if (prev) {
+void my_remove_from_free_list(my_metadata_t *metadata, my_metadata_t *prev)
+{
+  if (prev)
+  {
     prev->next = metadata->next;
-  } else {
+  }
+  else
+  {
     my_heap.free_head = metadata->next;
   }
   metadata->next = NULL;
@@ -64,7 +71,8 @@ void my_remove_from_free_list(my_metadata_t *metadata, my_metadata_t *prev) {
 //
 
 // This is called at the beginning of each challenge.
-void my_initialize() {
+void my_initialize()
+{
   my_heap.free_head = &my_heap.dummy;
   my_heap.dummy.size = 0;
   my_heap.dummy.next = NULL;
@@ -74,19 +82,30 @@ void my_initialize() {
 // |size| is guaranteed to be a multiple of 8 bytes and meets 8 <= |size| <=
 // 4000. You are not allowed to use any library functions other than
 // mmap_from_system() / munmap_to_system().
-void *my_malloc(size_t size) {
+void *my_malloc(size_t size)
+{
   my_metadata_t *metadata = my_heap.free_head;
   my_metadata_t *prev = NULL;
-  // First-fit: Find the first free slot the object fits.
-  // TODO: Update this logic to Best-fit!
-  while (metadata && metadata->size < size) {
+  my_metadata_t *metadata_best = metadata;
+  my_metadata_t *prev_best = prev;
+  size_t size_best = 0;
+
+  while (metadata)
+  {
+    if (metadata->size >= size && (size_best == 0 || size_best > metadata->size))
+    {
+      size_best = metadata->size;
+      metadata_best = metadata;
+      prev_best = prev;
+    }
     prev = metadata;
     metadata = metadata->next;
   }
   // now, metadata points to the first free slot
   // and prev is the previous entry.
 
-  if (!metadata) {
+  if (size_best == 0)
+  {
     // There was no free slot available. We need to request a new memory region
     // from the system by calling mmap_from_system().
     //
@@ -110,13 +129,14 @@ void *my_malloc(size_t size) {
   // ... | metadata | object | ...
   //     ^          ^
   //     metadata   ptr
-  void *ptr = metadata + 1;
-  size_t remaining_size = metadata->size - size;
-  metadata->size = size;
+  void *ptr = metadata_best + 1;
+  size_t remaining_size = metadata_best->size - size;
+  metadata_best->size = size;
   // Remove the free slot from the free list.
-  my_remove_from_free_list(metadata, prev);
+  my_remove_from_free_list(metadata_best, prev_best);
 
-  if (remaining_size > sizeof(my_metadata_t)) {
+  if (remaining_size > sizeof(my_metadata_t))
+  {
     // Create a new metadata for the remaining free slot.
     //
     // ... | metadata | object | metadata | free slot | ...
@@ -135,7 +155,8 @@ void *my_malloc(size_t size) {
 
 // This is called every time an object is freed.  You are not allowed to
 // use any library functions other than mmap_from_system / munmap_to_system.
-void my_free(void *ptr) {
+void my_free(void *ptr)
+{
   // Look up the metadata. The metadata is placed just prior to the object.
   //
   // ... | metadata | object | ...
@@ -147,12 +168,14 @@ void my_free(void *ptr) {
 }
 
 // This is called at the end of each challenge.
-void my_finalize() {
+void my_finalize()
+{
   // Nothing is here for now.
   // feel free to add something if you want!
 }
 
-void test() {
+void test()
+{
   // Implement here!
   assert(1 == 1); /* 1 is 1. That's always true! (You can remove this.) */
 }
